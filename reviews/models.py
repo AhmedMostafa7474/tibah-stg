379e4d1ea238f7e3436db3c1b034041ac0518a89
Ahmed Mostafa <kudo7474@gmail.com>
2024-08-19 12:47:13 +0300

    Add Validation

--- reviews/models.py
+++ reviews/models.py
@@ -1,13 +1,17 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 # Create your models here.
 class Review(models.Model):
-    course_key = models.CharField(max_length=200,null=True,blank=True)
-    course_name = models.CharField(max_length=200,null=True,blank=True)
-    username  = models.CharField(max_length=100,null=True,blank=True)
+    course_key = models.CharField(max_length=200)
+    course_name = models.CharField(max_length=200)
+    username  = models.CharField(max_length=100)
     image = models.ImageField(null=True,blank=True,upload_to='user-image')
     title = models.CharField(max_length=300,null=True,blank=True)
-    message = models.TextField(null=True,blank=True)
-    rate = models.DecimalField(max_digits=4, decimal_places=2,null=True,blank=True)
+    message = models.TextField()
+    rate = models.DecimalField(max_digits=4, decimal_places=2, validators=[
+            MinValueValidator(1.00),
+            MaxValueValidator(5.00)
+        ])
     def __str__(self):
         return self.username + " " + self.course_key
