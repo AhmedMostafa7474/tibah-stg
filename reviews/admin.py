d2397cb2059d78be6f78af44b6bd7714bd09b216
Ahmed Mostafa <kudo7474@gmail.com>
2024-08-26 15:25:58 +0300

    Add contact us admin

--- reviews/admin.py
+++ reviews/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from reviews.models import Review
+from reviews.models import Review,ContactUS
 from import_export.admin import ImportExportModelAdmin
 
 # Register your models here.
@@ -8,5 +8,10 @@ class ReviewAdmin(ImportExportModelAdmin):
     search_fields = ['course_key','course_name','username']
     list_filter = ('course_key','course_name','rate')
     list_display = ('course_key', 'course_name','username','message','rate')
-    
+
+class ContactUSAdmin(admin.ModelAdmin):
+    search_fields = ['name', 'email', 'phone']
+    list_display = ['name', 'email', 'phone', 'message']
+
 admin.site.register(Review,ReviewAdmin)
+admin.site.register(ContactUS,ContactUSAdmin)
