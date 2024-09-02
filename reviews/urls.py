dbdec7645f2c1a5e415e7eb623bac61763bcbc1a
Ahmed Mostafa <kudo7474@gmail.com>
2024-09-02 17:22:37 +0300

    edit url

--- reviews/urls.py
+++ reviews/urls.py
@@ -2,9 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-        path('<str:id>',views.ReviewView.as_view(), name='reviews'),
-        path('',views.ReviewView.as_view(), name='reviews'),
-        path('all',views.AllReviewsView.as_view(), name='reviews'),
-        path('contactus',views.ContactUSView.as_view(), name='ContactUSView'),
-
+        path('review/<str:id>/',views.ReviewView.as_view(), name='reviews'),
+        path('postreview/',views.ReviewView.as_view(), name='reviews'),
+        path('all/',views.AllReviewsView.as_view(), name='reviews'),
+        path('contactus/',views.ContactUSView.as_view(), name='ContactUSView'),
 ]
